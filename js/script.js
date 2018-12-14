6f6517280d3c9f13274e8bf528e1cdca7b553c54
krionick97 <mistermers8@gmail.com>
2018-12-14 19:47:15 +0300

    Main screen correction

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
 $(document).ready(function () {
   
-  $('.input-phone').mask('+7 (999) 99-999-99');
+  $('.input-phone').mask('+7 (999) 999-99-99');
 
   $('.header-mobile__btn-menu').click(function(e) {
     $(this).toggleClass('header-mobile__btn-menu_active');
     $('.modal-menu').toggleClass('modal-menu_visible');
   });
-
-  // $('.header-mobile__btn-menu').click(function () {
-  //   $('.modal-menu').addClass('modal-menu_visible');
-  // });
-  // $('.header-mobile__btn-menu').click(function () {
-  //   $('.modal-menu').removeClass('modal-menu_visible');
-  // });
 
   // $('.link').click(function () {return false;});
 
