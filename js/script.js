8902e1d821000313d8ef2ae80d5ed1505fd834c5
krionick97 <mistermers8@gmail.com>
2018-12-15 22:02:48 +0300

    5th block

--- js/script.js
+++ js/script.js
@@ -15,6 +15,44 @@ $(document).ready(function () {
     slidesToScroll: 1,
     nextArrow: '<div class="arrow arrow-right"></div>',
     prevArrow: '<div class="arrow arrow-left"></div>'
-  }); //slider
+  }); //slider real-projects
+  
+  $('.our-clients__logos').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: '<div class="arrow arrow-right"></div>',
+    prevArrow: '<div class="arrow arrow-left"></div>',
+    responsive: [
+      {
+        breakpoint: 1060,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  }); //slider logo-clients
+
+
 
 });
