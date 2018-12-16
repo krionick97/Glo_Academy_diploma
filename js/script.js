4092bc45c4e4c4986045a01a943089ab1e22cbb4
krionick97 <mistermers8@gmail.com>
2018-12-16 20:24:39 +0300

    Modals

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
+  $('.modal-menu').hide();
   
   $('.input-phone').mask('+7 (999) 999-99-99');
 
-  $('.header-mobile__btn-menu').click(function(e) {
-    $(this).toggleClass('header-mobile__btn-menu_active');
-    $('.modal-menu').toggleClass('modal-menu_visible');
-  });
-  
+  // $('.header-mobile__btn-menu').click(function(e) {
+  //   $(this).toggleClass('header-mobile__btn-menu_active');
+  //   $('.modal-menu').toggleClass('modal-menu_visible');
+  //   // $('.link').toggleClass('modal-menu_visible');
+  // });
+
   // $('.link').click(function () {return false;});
 
   $('.real-projects__slider').slick({
@@ -53,6 +55,35 @@ $(document).ready(function () {
     ]
   }); //slider logo-clients
 
+  $('.header__callback').click(function() {
+    $('.modal-callback').addClass('modal-callback_visible');
+    return false;
+  });
+
+  $('.var-maquette__button, .real-projects__slider-button').click(function() {
+    $('.order-maq').addClass('order-maq_visible');
+    return false;
+  });
+  
+  
+  $('.header-mobile__btn-menu').click(function() {
+    $('.modal-menu').show();
+  });
+  // $('.header-mobile__btn-menu').click(function() {
+  //   $('.modal-menu').addClass('modal-menu_visible');
+  //   return false
+  // });
+
+  $('.modal__close').click(function() {
+    $('.modal-callback').removeClass('modal-callback_visible');
+    $('.order-maq').removeClass('order-maq_visible');
+    return false;    
+  }); //close modal
+
+  $('.modal-menu__close, .modal-menu__menu a').click(function() {
+    $('.modal-menu').hide();
+  });
 
 
-});
+  
+}); //Document Ready
